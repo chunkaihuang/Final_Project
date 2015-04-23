81fe1d3c8aee5681db79dd7672f0edfcffd83044
oscar <ojingham@gmail.com>
2015-04-23 19:05:57 -0400

    added branch

--- group1/communication/urls.py
+++ group1/communication/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url, include
+from comm import views
 
 urlpatterns = patterns('',
-                       # url(r'^admin/', include(admin.site.urls)),
-                       )
+    url(r'^$', views.index, name='index'),
+)
